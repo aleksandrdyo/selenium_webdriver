d43352e57a781fd78132ddf644f99ded9d2b7159
dyo999 <dyoalex@gmail.com>
2022-04-26 00:26:12 +0300

    add_auth_jira

--- selenium_course/jira.py
+++
@@ -1,5 +0,0 @@
-from jira import JIRA
-
-jira = JIRA()
-
-jac = JIRA('https://dyo999.atlassian.net/jira/')
---
+++ selenium_course/jira_auth.py
@@ -0,0 +1,5 @@
+import re
+from jira import JIRA
+
+jira = JIRA('https://dyo999.atlassian.net/',
+            basic_auth=('dyoalex@gmail.com', '755FzZM9urOqP8TlPmqO52D2'))
