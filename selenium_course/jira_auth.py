2204d03c2a02038b0da27919de3632620a0f3dab
dyo999 <dyoalex@gmail.com>
2022-04-26 00:34:16 +0300

    del_token

--- selenium_course/jira_auth.py
+++ selenium_course/jira_auth.py
@@ -2,4 +2,4 @@ import re
 from jira import JIRA
 
 jira = JIRA('https://dyo999.atlassian.net/',
-            basic_auth=('dyoalex@gmail.com', '755FzZM9urOqP8TlPmqO52D2'))
+            basic_auth=('dyoalex@gmail.com', ''))
