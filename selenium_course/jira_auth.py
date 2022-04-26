8feaaaa8217ff7f160dea1212c0c70506b8ee9ab
dyo999 <dyoalex@gmail.com>
2022-04-26 10:07:19 +0300

    add_get_issue

--- selenium_course/jira_auth.py
+++ selenium_course/jira_auth.py
@@ -1,5 +1,14 @@
 import re
 from jira import JIRA
+from requests_toolbelt import user_agent
 
 jira = JIRA('https://dyo999.atlassian.net/',
-            basic_auth=('dyoalex@gmail.com', ''))
+            basic_auth=('dyoalex@gmail.com', ''),
+            options={"headers": {"User-Agent": user_agent("my_package", "0.0.1")}}
+            )
+
+
+
+issue = jira.issue('DYO-1', fields='summary,comment')
+
+summary = issue.fields.summary         # 'Field level security permissions'
