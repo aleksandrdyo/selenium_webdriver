6684926bb5f7340d5ab25b1a9d081a90440aac8b
dyo999 <dyoalex@gmail.com>
2022-04-30 01:06:11 +0300

    add_func_get_issues

--- selenium_course/jira_auth.py
+++ selenium_course/jira_auth.py
@@ -7,8 +7,19 @@ jira = JIRA('https://dyo999.atlassian.net/',
             options={"headers": {"User-Agent": user_agent("my_package", "0.0.1")}}
             )
 
+project = 'project = dyo'
+
+def get_my_open_tasks():
+    issues1 = jira.search_issues(project)
+    for x in range(len(issues1)):
+        print(issues1[x])
+
+get_my_open_tasks()
 
 
-issue = jira.issue('DYO-1', fields='summary,comment')
-
-summary = issue.fields.summary         # 'Field level security permissions'
+issue = jira.issue('DYO-1')
+print(issue.fields.project.key)
+print(issue.fields.issuetype.name)
+print(issue.fields.reporter.displayName)
+print(issue.fields.summary)
+print(issue.fields.comment.comments)
