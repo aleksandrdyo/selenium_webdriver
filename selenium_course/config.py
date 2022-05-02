cf11d3cc58c26bf84e853f77091e74b501f381cd
dyo999 <dyoalex@gmail.com>
2022-05-02 23:29:26 +0200

    Добавил конфигурационный файл с константами, сделал импорт констант в файл jira_auth

---
+++ selenium_course/config.py
@@ -0,0 +1,5 @@
+TEST = "TEST_VAR"
+JIRA_EP = "https://dyo999.atlassian.net/"
+EMAIL = "dyoalex@gmail.com"
+TOKEN = "6fKP6m3nVCKh8sRT4oae23C1"
+PROJECT_NM = 'project = dyo'
--- selenium_course/jira_auth.py
+++ selenium_course/jira_auth.py
@@ -1,16 +1,14 @@
-import re
 from jira import JIRA
 from requests_toolbelt import user_agent
+from config import JIRA_EP, EMAIL, TOKEN, PROJECT_NM
 
-jira = JIRA('https://dyo999.atlassian.net/',
-            basic_auth=('dyoalex@gmail.com', ''),
+jira = JIRA(JIRA_EP,
+            basic_auth=(EMAIL, TOKEN),
             options={"headers": {"User-Agent": user_agent("my_package", "0.0.1")}}
             )
 
-project = 'project = dyo'
-
 def get_my_open_tasks():
-    issues = jira.search_issues(project)
+    issues = jira.search_issues(PROJECT_NM)
     for x in range(len(issues)):
         print(issues[x])
 
